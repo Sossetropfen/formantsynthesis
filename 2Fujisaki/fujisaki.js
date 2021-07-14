6ba3ac0d0f3b34cf06bac81f6a1af8dea647da68
k0nst1 <k.prieb@campus.tu-berlin.de>
2021-07-14 21:09:46 +0200

    added fujisaki sample values, little fixes

--- 2Fujisaki/fujisaki.js
+++ 2Fujisaki/fujisaki.js
@@ -1,8 +1,8 @@
 
 /* -------------------------Variables and arrays-------------------------*/
 var bias = 0;  // bias level of instance - Fb
-var phr_cmd = 0; // Number of phrase-controls - I
-var acc_cmd = 0; // Number of accent-controls - J
+var phr_cmd = 0; // Number of phrase-commands - I
+var acc_cmd = 0; // Number of accent-commands - J
 var A_pi = []; // magnitude levels of phrase commands - A_pi
 var A_aj = []; // amplitude levels of accent commands 
 var T_0i = []; // Instants of prhase commands
@@ -12,36 +12,14 @@ var a_i = []; // nat. angular freq. of phrase-control to phrase commands
 var b_j = []; // nat. angular freq. of accent-control to accent commands
 var v_j = []; // ceilling level of accent component in accent commands
 
-/*
-var phr_cmd = 3;
-var acc_cmd = 5;
-var A_pi = [0.4, 0.5, 0.4];
-var T_0i = [0, 1.8, 3.8];
-var bias = 1;
-var A_aj = [0.2,0.3,0.4,0.5,0.3];
-var T_1j = [0.25,1.25,2.25,3,4.6];
-var T_2j = [0.75,1.75,2.6,3.2,4.85];
-var v_j = [1,1,1,1,1];
-
-var a_i = [2.5,3,3];
-var b_j = [1,1,1,1,1];
-*/
-
-
 /*-------------------------Variables end----------------------------------*/
-
-
-/*-------------------------Event Listeners--------------------------------*/
-
-/*-------------------------Event Listeners end----------------------------*/
 
 
 /*-------------------------Functions--------------------------------------*/
 function BiasInput(){
       bias = document.getElementById("bias_input").value;
       document.getElementById("btn_bias").innerText = "change bias level";
-      //alert(bias);
-};
+}
 
 function PhraseInput(){
       phr_cmd++;
@@ -49,11 +27,7 @@ function PhraseInput(){
       T_0i.push(document.getElementById("T_0i_input").value);
       a_i.push(document.getElementById("a_i_input").value);
       document.getElementById("btn_phrase").innerText = "add phrase command";
-      //alert(phr_cmd);
-      //alert(A_pi);
-      //alert(T_0i);
-      //alert(a_i);
-};
+}
 
 function AccentInput(){
       acc_cmd++;
@@ -63,36 +37,58 @@ function AccentInput(){
       b_j.push(document.getElementById("b_j_input").value);
       v_j.push(document.getElementById("v_j_input").value);
       document.getElementById("btn_accent").innerText = "add accent command";
-      //alert(acc_cmd);
-      //alert(A_ai);
-      //alert(T_1j);
-      //alert(T_2j);
-      //alert(b_j);
-      //alert(v_j);
-};
-/*
-function PlotOutput(){
-    console.log("oke");
-    console.log("bias: "+bias);
-    console.log(acc_cmd + " accent commands: ");
-    console.log(A_aj);
-    console.log(T_1j);
-    console.log(T_2j);
-    console.log(b_j);
-    console.log(v_j);
-    console.log(phr_cmd + " phrase commands: ");
+}
+
+/*function show(){
+    console.log(phr_cmd);
     console.log(A_pi);
     console.log(T_0i);
     console.log(a_i);
-    
-      //here's some stuff to do!!!!!!!!!!!!!!!!!!!!!!
+    console.log(acc_cmd);
+    console.log(A_aj);
+    console.log(T_1j);
+    console.log(T_2j);
+    console.log(v_j);
+    console.log(b_j);
+}*/
 
-};
-*/
+function loadSampleValues(){
+    phr_cmd = 3;
+    acc_cmd = 5;
+    A_pi = [0.4, 0.5, 0.4];
+    T_0i = [0, 1.8, 3.8];
+    bias = 1;
+    A_aj = [0.2,0.3,0.4,0.5,0.3];
+    T_1j = [0.25,1.25,2.25,3,4.6];
+    T_2j = [0.75,1.75,2.6,3.2,4.85];
+    v_j = [1,1,1,1,1];
+
+    a_i = [2.5,3,3];
+    b_j = [1,1,1,1,1];
+}
+
+function reset(){
+    bias = 0;
+    phr_cmd = 0;
+    acc_cmd = 0;
+    A_pi = [];
+    A_aj = [];
+    T_0i = [];
+    T_1j = [];
+    T_2j = [];
+    a_i = [];
+    b_j = [];
+    v_j = [];
+    document.getElementById("btn_phrase").innerText = "submit phrase command";
+    document.getElementById("btn_accent").innerText = "submit accent command";
+    document.getElementById("btn_bias").innerText = "enter bias level";
+}
+
 function fct(t){
-    //return /*Math.log(bias)*/phraseCmds(t)/* + accentCmds(t)*/;
+    if (bias <= 0){
+        return accentCmds(t)+phraseCmds(t);
+    }
     return Math.log(bias)+accentCmds(t)+phraseCmds(t);
-	//return phraseCmds(t);
 }
 
 function accentCmds(t){
@@ -114,7 +110,7 @@ function phraseCmds(t){
 }
 
 function G_p_i(i,t){
-    if (t < 0){
+    if (t <= 0){
         return 0;
     } else {
 		return Math.pow(a_i[i],2)*t*Math.exp(-a_i[i]*t);
@@ -131,7 +127,6 @@ function G_a_j(j,t){
 function accentCmdsDraw(t){
     var val = 0; 
 	for(var j = 0; j < acc_cmd; j++){
-		//console.log(T_1j[j],T_2j[j],t,A_aj[j]);
 		val += rechteck(T_1j[j],T_2j[j],t,A_aj[j]);
 	}
 	return val;
